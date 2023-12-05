58c79a80d59a21b6eb9af869ea7b9b89e3e290d1
User <User@DESKTOP-HJ5EQC9>
2023-12-05 14:46:04 +0000

    Add project files.

---
+++ WikiProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WikiProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
